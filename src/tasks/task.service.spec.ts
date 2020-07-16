df3c89445bb454efb345519eef471b50f21c1704
Johnny Choudhury-Lucas <496597+jchlu@users.noreply.github.com>
2020-07-16 16:46:02 -0400

    Initial TaskService testing, giving an error

--- src/tasks/task.service.spec.ts
+++ src/tasks/task.service.spec.ts
@@ -1,10 +1,14 @@
 import { Test } from '@nestjs/testing'
 import { TasksService } from './tasks.service'
 import { TaskRepository } from './task.repository'
+import { GetTasksFilterDto } from './get-tasks-filter.dto'
+import { TaskStatus } from './task.status.enum'
 
 const mockTaskRepository = () => ({
   getTasks: jest.fn(),
 })
+
+const mockUser = { username: 'Testy McTesting' }
 
 describe('TasksService', () => {
   let tasksService: TasksService
@@ -27,6 +31,11 @@ describe('TasksService', () => {
       // expect getTasks NOT to have been called
       expect(taskRepository.getTasks).not.toHaveBeenCalled()
       // call taskService.getTasks()
+      const filters: GetTasksFilterDto = {
+        status: TaskStatus.IN_PROGRESS,
+        search: 'Test search',
+      }
+      tasksService.getTasks(filters, mockUser)
       // expect getTasks TO HAVE BEEN CALLED
     })
   })
